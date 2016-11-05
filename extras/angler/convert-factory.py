1160a969476fd5b11e4126a36ff16c58fc5becee
Mike Perry <mikeperry-git@torproject.org>
2016-11-05 16:32:25 -0700

    Add original source file name for the convert-factory.py script.

--- extras/angler/convert-factory.py
+++ extras/angler/convert-factory.py
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+#
+# Based on device_huawei_angler/releasetools.py.
+# Portions trimmed to remove deltas and zip file reading.
 
 import common
 import struct
