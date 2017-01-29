af260aa0cddfdf9b43c0abfd77932a5016180378
Mike Perry <mikeperry-git@torproject.org>
2017-01-28 22:58:43 -0800

    Slighly improve clone-helper-repos.sh to catch more failures.

--- clone-helper-repos.sh
+++ clone-helper-repos.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+set -e
 
 echo "Cloning android-simg2img and super-bootimg forks..."
 
 SUPERBOOT_DIR=$1
 SIMG2IMG_DIR=$2
 
-
 if [ ! -d $SIMG2IMG_DIR ]
 then
   git clone -b verity_tools3 https://github.com/mikeperry-tor/android-simg2img $SIMG2IMG_DIR
-  cd $SIMG2IMG_DIR
-  make
-  cd -
 fi
+
+# XXX: Maybe update git here?
+cd $SIMG2IMG_DIR
+make
+cd -
 
 # XXX: Should add something to check for a different origin/branch/checkout and fix it
 if [ ! -d $SUPERBOOT_DIR ]
